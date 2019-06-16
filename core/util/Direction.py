c21ff1d01d8f4ebe5e7a2657393d0a50f8593537
Felix Maaß <felix@maass24.de>
2019-06-16 16:09:51 +0200

    Add fromInt initializer to Direction Enum for use with neural network

--- core/util/Direction.py
+++ core/util/Direction.py
@@ -10,4 +10,14 @@ class Direction(Enum):
     DOWN_LEFT = 'DOWN_LEFT'
     LEFT = 'LEFT'
     UP_LEFT = 'UP_LEFT'
-        
+
+    @staticmethod
+    def fromInt(value):
+        if not isinstance(value, int):
+            raise ValueError('Argument is not an int: ' + str(value))
+
+        for i, dir in enumerate(Direction):
+            if i == value:
+                return dir
+
+        raise ValueError('Argument is not in range of 0...7: ' + str(value))
