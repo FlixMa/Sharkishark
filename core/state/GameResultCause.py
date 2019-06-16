400c38b5a8824d73cdfda7977e8b5ddad56f8127
Felix Maaß <felix@maass24.de>
2019-06-16 17:55:17 +0200

    Add Parsing of GameResultCause

--- core/state/GameResultCause.py
+++ core/state/GameResultCause.py
@@ -2,6 +2,26 @@ from enum import Enum
 
 class GameResultCause(Enum):
     REGULAR         = 0
-    RULE_VIOLATION  = 1
-    SOFT_TIMEOUT    = 2
-    HARD_TIMEOUT    = 3
+    LEFT            = 1
+    RULE_VIOLATION  = 2
+    SOFT_TIMEOUT    = 3
+    HARD_TIMEOUT    = 4
+
+    @classmethod
+    def fromString(cls, causeString):
+        if not isinstance(causeString, str):
+            return None
+
+        causeString = causeString.upper()
+        if causeString == 'REGULAR':
+            return cls.REGULAR
+        elif causeString == 'LEFT':
+            return cls.LEFT
+        elif causeString == 'RULE_VIOLATION':
+            return cls.RULE_VIOLATION
+        elif causeString == 'SOFT_TIMEOUT':
+            return cls.SOFT_TIMEOUT
+        elif causeString == 'HARD_TIMEOUT':
+            return cls.HARD_TIMEOUT
+        else:
+            return None
